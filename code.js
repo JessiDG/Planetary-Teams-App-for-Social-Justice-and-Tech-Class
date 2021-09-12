4200cef0292777ba2adb5eea41702bc5935e2108
Jessica Dickinson Goodman <jessica.dickinson.goodman@gmail.com>
2021-09-11 20:28:21 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,66 @@
+onEvent("mercuryButton", "click", function( ) {
+  setScreen("Mercury");
+});
+onEvent("earthButton", "click", function( ) {
+  setScreen("Earth");
+});
+onEvent("jupiterButton", "click", function( ) {
+  setScreen("Jupiter");
+});
+onEvent("saturnButton", "click", function( ) {
+  setScreen("Saturn");
+});
+onEvent("earthScreenButton", "click", function( ) {
+  open("https://www.nasa.gov/topics/earth/images/index.html");
+});
+onEvent("jupiterScreenButton", "click", function( ) {
+  open("https://www.nasa.gov/mission_pages/juno/images/index.html");
+});
+onEvent("mercuryScreenButton", "click", function( ) {
+  open("https://www.nasa.gov/planetmercury");
+});
+onEvent("saturnScreenButton", "click", function( ) {
+  open("https://solarsystem.nasa.gov/planets/saturn/galleries/?page=0&per_page=25&order=created_at+desc&search=&href_query_params=category%3Dplanets_saturn&button_class=big_more_button&tags=saturn&condition_1=1%3Ais_in_resource_list&category=51");
+});
+onEvent("button1", "click", function( ) {
+  setScreen("Saturn");
+});
+onEvent("backButtonEarth", "click", function( ) {
+  setScreen("Home");
+});
+onEvent("backButtonJupiter", "click", function( ) {
+  setScreen("Home");
+});
+onEvent("backButtonMercury", "click", function( ) {
+  setScreen("Home");
+});
+onEvent("backButtonSaturn", "click", function( ) {
+  setScreen("Home");
+});
+onEvent("saturn_app", "click", function( ) {
+  open("https://studio.code.org/projects/applab/Itpmvumn9BD0jtzhRw1j3qNjgFNR19ARhT0iN6rOufg");
+});
+onEvent("saturn_website", "click", function( ) {
+  open("https://makenabrowning.wixsite.com/my-site");
+});
+onEvent("mercury_app", "click", function( ) {
+  open("https://studio.code.org/projects/applab/2nJzgRD7Eh9tP3CczCnliWYx6dj8Q3o9nbRmSlJiHHw");
+});
+onEvent("mercury_website", "click", function( ) {
+  open("https://ashleykyurem.wixsite.com/blurthelines");
+});
+onEvent("earth_app", "click", function( ) {
+  open("https://studio.code.org/projects/applab/ANCsMZYqLzQljoID1gFtfIvG50l5fdxOYg4OtzGGwQQ");
+});
+onEvent("earth_app2", "click", function( ) {
+  open("https://studio.code.org/projects/applab/dJKUl0QoBaELVyWqaONtsjH-yPxIG40cdcNLqk4plZM");
+});
+onEvent("earth_website", "click", function( ) {
+  open("https://kevinshiah503.wixsite.com/my-site");
+});
+onEvent("jupiter_app", "click", function( ) {
+  open("https://studio.code.org/projects/applab/GG-ZMw8aOAXzdn-Ef5RrXMTGBo7XnK_b2JnIuwPQfh4");
+});
+onEvent("jupiter_website", "click", function( ) {
+  open("https://diegoalcala0330.wixsite.com/traxcess");
+});
